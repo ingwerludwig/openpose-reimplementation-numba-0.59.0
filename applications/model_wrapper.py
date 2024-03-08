397b33c14eabf50ffd61e18d7a4893dba310067e
Ingwer Ludwig <ingwerludwig@Ingwers-MacBook-Air.local>
2024-03-08 17:11:12 +0700

    feat: reimplement official OpenPose with numba 0.59.0

---
+++ applications/model_wrapper.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+post_dir = os.path.join(current_dir, '..')
+absolute_post_dir = os.path.abspath(post_dir)
+sys.path.append(absolute_post_dir)
+print(absolute_post_dir)
+
+import post_processing.post as post
+import configs.post_config as post_config
+import configs.keypoints_config as kpts_config
+import configs.default_config as def_config
+
+post.Skeletonizer.config(kpts_config.KEYPOINTS_DEF, kpts_config.JOINTS_DEF, post_config)
+post.Skeleton.config(kpts_config.KEYPOINTS_DEF, kpts_config.JOINTS_DEF)
+
+
+class ModelWrapper:
+    def __init__(self, model_path):
+        self.model = tf.keras.models.load_model(model_path)
+
+    def process_image(self, img):
+        input_img = tf.image.resize(img, (def_config.IMAGE_HEIGHT, def_config.IMAGE_WIDTH))
+        input_img = tf.image.convert_image_dtype(input_img, dtype=tf.float32)
+        input_img /= 255
+        input_img = input_img[tf.newaxis, ...]
+        pafs, kpts = self.model.predict(input_img)
+        pafs = pafs[0]
+        kpts = kpts[0]
+        skeletonizer = post.Skeletonizer(kpts, pafs)
+        skeletons = skeletonizer.create_skeletons()
+        return skeletons
